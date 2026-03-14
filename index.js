52bf978be2d8fc610a1db792b28b940a07772927
April Lindie <april@MacBookPro.lan>
2026-03-13 22:59:43 -0400

    Completed Flatburger scope lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+//// ----- Global Variables -----
+let burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+
+// ----- Function: Add a Burger -----
+function addBurger() {
+
+  // function-scoped variable
+  let newBurger = "Flatburger";
+
+  // add burger to the menu array
+  burgers.push(newBurger);
+
+}
+
+
+// ----- Block Scope Example -----
+if (true) {
+
+  // block-scoped variable
+  let anotherNewBurger = "Maple Bacon Burger";
+
+  // update the burger list
+  burgers.push(anotherNewBurger);
+
+}
+
+
+// ----- Function: Change Featured Drink -----
+function changeFeaturedDrink() {
+
+  // update the global drink value
+  featuredDrink = "The JavaShake";
+
+}
